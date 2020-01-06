9dbc332f336afbd4127ada5fb357ee407bcb42f6
David Martos <david.martos9.7@gmail.com>
2020-01-06 22:40:30 +0100

    Working...

--- easy_dial.cpp
+++ easy_dial.cpp
@@ -1,1 +1,10 @@
 #include "easy_dial.hpp"
+
+easy_dial::easy_dial(const call_registry& R) throw(error){
+  vector<phone> aux;
+  R.dump(aux);
+  arrel = new node;
+  for(int i=0 ; i<aux.size() ; i++){
+    arrel = insereix_paraula(arrel,0,aux[i].nom(),aux[i].numero());
+  }
+}
